e08db42b05c3acec0839b11b03d9f7a17623e216
adambonneruk <adam@bonner.uk>
2020-09-02 23:22:24 +0100

    moving away from makefile and commenting code

--- rot13.c
+++ rot13.c
@@ -2,27 +2,35 @@
 #include <string.h>
 #define ROT 13
 
-main (argc,argv)
+/***************************************************************************************************
+argv and argc are how command line arguments are passed to main() in C and C++. argc will be the
+number of strings pointed to by argv. This will (in practice) be 1 plus the number of arguments, as
+virtually all implementations will prepend the name of the program to the array.
 
-int argc;
-char *argv[];
+The variables are named argc (argument count) and argv (argument vector) by convention, but they can
+be given any valid identifier: int main(int num_args, char** arg_strings) is equally valid.
 
+They can also be omitted entirely, yielding int main(), if you do not intend to process command line
+arguments.
+***************************************************************************************************/
+
+int main (int argc, char const *argv[])
 {
-	int go;
+	int i;
 	
 	printf("\nROT13 Encryption Produces This Result:\n");
 
-	for (go=1; go<=argc-1; go++)
+	for (i=1; i<=argc-1; i++)
 	{
 		int count;
 		int rotlen;	
 
-		rotlen = strlen(argv[go]);	
+		rotlen = strlen(argv[i]);	
 
 		char rotstr[rotlen];
 		char rotted[rotlen];
 
-		strcpy(rotstr, argv[go]);
+		strcpy(rotstr, argv[i]);
 	
 		for (count=0; count<=rotlen; count++)
 		{
@@ -45,4 +53,6 @@ char *argv[];
 	}
 	
 	printf("\n\n");
+
+return 0;
 }
