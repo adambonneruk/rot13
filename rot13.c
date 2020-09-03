800e54313557c3b611fbf44650106c90e7da83d6
adambonneruk <adam@bonner.uk>
2020-09-03 02:37:41 +0100

    working rot47 code

--- rot13.c
+++ rot13.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void rotateWords(int wordIndex, char const *wordArray[], int mode);
+void rotateWords(int wordIndex, char const *wordArray[], int rotateMode);
 
 int main(int argc, char const *argv[]) {
 	if (strcmp("--47",argv[1]) == 0) { //first argument is the 47 flag
@@ -16,14 +16,14 @@ int main(int argc, char const *argv[]) {
 	return 0;
 }
 
-void rotateWords(int wordIndex, char const *wordArray[], int mode) {
+void rotateWords(int wordIndex, char const *wordArray[], int rotateMode) {
 	int wordLength = strlen(wordArray[wordIndex]);
 	char word[wordLength];
 	char rotatedWord[wordLength];
 	strcpy(word, wordArray[wordIndex]);
 	for (int letterIndex = 0; letterIndex <= wordLength - 1; letterIndex++)	{
-		//if (letterIndex == 0 && wordIndex != 1) {printf(" ");} //hack to put space infront of all words except first
-		if (mode == 13) { //default/normal
+		if (rotateMode == 13) { //default/normal
+			if (letterIndex == 0 && wordIndex != 1) {printf(" ");} //hack to put space infront of all words except first
 			if (((word[letterIndex]>='a')&&(word[letterIndex]<='m'))||((word[letterIndex]>='A')&&(word[letterIndex]<='M'))) {
 				rotatedWord[letterIndex] = word[letterIndex] + 13;
 			} else if (((word[letterIndex]>='n')&&(word[letterIndex]<='z'))||((word[letterIndex]>='N')&&(word[letterIndex]<='Z'))) {
@@ -32,6 +32,7 @@ void rotateWords(int wordIndex, char const *wordArray[], int mode) {
 				rotatedWord[letterIndex] = word[letterIndex];
 			}
 		} else { //it's 47
+			if (letterIndex == 0 && wordIndex != 2) {printf(" ");} //hack to put space infront of all words except first
 			if ((word[letterIndex]>=33)&&(word[letterIndex]<=79)) {
 				rotatedWord[letterIndex] = word[letterIndex] + 47;
 			} else if ((word[letterIndex]>=80)&&(word[letterIndex]<=126)) {
@@ -40,8 +41,9 @@ void rotateWords(int wordIndex, char const *wordArray[], int mode) {
 				rotatedWord[letterIndex] = word[letterIndex];
 			}
 		}
-		printf("Word: %s\t Letter Index: %d\t Letter: %c\t Rotated Letter: %c\n",word,letterIndex + 1,word[letterIndex],rotatedWord[letterIndex]);
-		//printf("%c",rotatedWord[letterIndex]);
+		//printf("Letter: %c\t Rotated Letter: %c\n",word[letterIndex],rotatedWord[letterIndex]);
+		//printf("Word: %s\t Letter Index: %d\t Letter: %c\t Rotated Letter: %c\n",word,letterIndex + 1,word[letterIndex],rotatedWord[letterIndex]);
+		printf("%c",rotatedWord[letterIndex]);
 	}
-	printf("wordArray[%d]: %s\t wordLength: %d\t Word: %s\n", wordIndex, wordArray[wordIndex], wordLength, word); //print a pretty array of the words
+	//printf("wordArray[%d]: %s\t wordLength: %d\t Word: %s\n", wordIndex, wordArray[wordIndex], wordLength, word); //print a pretty array of the words
 }
